92bf8f09c27a910acceffb9bd2cdbe568e482d84
Ahsan Khan <imkhanahsan@yahoo.com>
2023-02-10 00:47:08 +0500

    added navbar

---
+++ components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='font-space max-w-[1240px] m-auto p-4'>
+            <div>
+                <h1 className='font-bold text-7xl text-text1'>Nice to meet you!
+                    <br />
+                    I'm <span className='underline underline-offset-[15px] decoration-4 decoration-accent'>Ahsan Khan</span>
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
