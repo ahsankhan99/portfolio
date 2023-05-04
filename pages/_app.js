668c3db45cfc2704316f7dad813d264d04a70b68
Ahsan Khan <imkhanahsan@yahoo.com>
2023-05-05 04:54:30 +0500

    add custom scrollbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 import Navbar from '../components/Navbar'
 import '@/styles/globals.css'
-import Fade from 'react-reveal/Fade';
+import '../styles/scrollbar.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {/* <Fade> */}
       <Navbar />
       <Component {...pageProps} />
-      {/* </Fade> */}
     </>
   )
 }
